ec3de3a13f3b481d59f62adae2e61c6aa352ba63
jalen4 <jmas0418@students.vsu.edu>
2025-02-25 09:52:01 -0500

    implementation

---
+++ FlixsterMovie/app/src/main/java/com/codepath/flixstermovie/Movie.kt
@@ -0,0 +1,14 @@
+package com.sarcb.flixsterplus
+
+import com.google.gson.annotations.SerializedName
+
+class Movie {
+    @SerializedName("title")
+    var title: String? = null
+
+    @SerializedName("overview")
+    var description: String? = null
+
+    @SerializedName("poster_path")
+    var posterURL: String? = null
+}
